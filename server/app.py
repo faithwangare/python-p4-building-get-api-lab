d08433fd84b4b3ad8b10f180223f150b5b4cbc0d
faithwangare <faithwangare750@gmail.com>
2023-06-26 14:10:32 +0300

    complete lab

--- server/app.py
+++ server/app.py
@@ -2,13 +2,12 @@
 
 from flask import Flask, make_response, jsonify
 from flask_migrate import Migrate
-
 from models import db, Bakery, BakedGood
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
 
 migrate = Migrate(app, db)
 
@@ -20,19 +19,63 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    bakeries = Bakery.query.all()
+    bakeries_serialized = [bakery.to_dict() for bakery in bakeries]
+
+    response = make_response(
+        jsonify(bakeries_serialized),
+        200
+    )
+    response.headers['Content-Type'] = 'application/json'
+    return response
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.get(id)
+    if bakery:
+        bakery_serialized = bakery.to_dict()
+        response = make_response(
+            jsonify(bakery_serialized),
+            200
+        )
+    else:
+        response = make_response(
+            jsonify({'error': 'Bakery not found'}),
+            404
+        )
+
+    response.headers['Content-Type'] = 'application/json'
+    return response
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    baked_goods = BakedGood.query.order_by(BakedGood.price).all()
+    baked_goods_serialized = [bg.to_dict() for bg in baked_goods]
+
+    response = make_response(
+        jsonify(baked_goods_serialized),
+        200
+    )
+    response.headers['Content-Type'] = 'application/json'
+    return response
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    most_expensive = BakedGood.query.order_by(BakedGood.price.desc()).first()
+    if most_expensive:
+        most_expensive_serialized = most_expensive.to_dict()
+        response = make_response(
+            jsonify(most_expensive_serialized),
+            200
+        )
+    else:
+        response = make_response(
+            jsonify({'error': 'No baked goods found'}),
+            404
+        )
+
+    response.headers['Content-Type'] = 'application/json'
+    return response
 
 if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+    app.run(port=555, debug=True)
